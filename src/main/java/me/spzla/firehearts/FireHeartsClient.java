b53c27e4bb23adcded6e583af7dc3022f4185eaa
spzla <57840318+spzla@users.noreply.github.com>
2024-06-20 14:29:10 +0200

    init

---
+++ src/main/java/me/spzla/firehearts/FireHeartsClient.java
@@ -0,0 +1,11 @@
+package me.spzla.firehearts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FireHeartsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
